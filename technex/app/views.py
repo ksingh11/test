29a4d863457143563b22e63658260b887e68c68c
Pranjal Mittal <mittal.pranjal@gmail.com>
2011-09-24 23:47:55 +0530

    basic views for team & event registration created along with trivial templates for the same

--- technex/app/views.py
+++ technex/app/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 #from technex.app.models import UserProfile, College, Team, Event, EventNotification, GeneralNotification
 from technex.app.models import *
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse, Http404
-#from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from simplejson import dumps
 
@@ -12,6 +12,16 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = UserProfile
         fields = ('name', 'username', 'password1', 'password2', 'email', 'contact', 'gender', 'college',)
+
+class TeamRegistrationForm(ModelForm):
+    class Meta:
+        model = Team
+        #fields = ('name', 'name', 'name', 'name', 'name',)
+
+class EventRegistrationForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ('name', 'participant_teams',)
 
 def index(request):
     #Registration Check
@@ -38,6 +48,45 @@ def index(request):
     
     return render_to_response('index.html', template_data, context_instance=RequestContext(request))
 
+def team_registration(request):
+    #Team Registration form generator & validator
+    error = False
+    reg_success = False
+    if request.method == "POST":
+        form = TeamRegistrationForm(request.POST)
+        if form.is_valid():         
+            form.save()
+            reg_success = True
+        else:
+            error = True
+    else:
+        form = TeamRegistrationForm()
+    template_data = {
+        'form': form,
+        'error': error,
+        'registration_successful': reg_success
+    }
+    return render_to_response('registration/registration_team.html', template_data, context_instance=RequestContext(request))
+
+def event_registration(request):
+    #Event Registration form generator & validator
+    error = False
+    reg_success = False
+    if request.method == "POST":
+        form = EventRegistrationForm(request.POST)
+        if form.is_valid():         
+            form.save()
+            reg_success = True
+        else:
+            error = True
+    else:
+        form = EventRegistrationForm()
+    template_data = {
+        'form': form,
+        'error': error,
+        'registration_successful': reg_success
+    }
+    return render_to_response('registration/registration_event.html', template_data, context_instance=RequestContext(request))
 
 def serialize_to_json(request):
     try:
@@ -58,15 +107,14 @@ def serialize_to_json(request):
     
 @login_required
 def my_page(request):
-    u = UserProfile.objects.get(request.user)
-    team_set = u.team_set.all()
-    for i in range(0,len(team_set)):
-        event_set = team_set[i].event_set.all()
-        for j in range(0, len(event_set)):
-            if j==0:
-                eventnotif_set = event_set[0].eventnotification_set.all()
-            else:
-                eventnotif_set +=  event_set[j].eventnotification_set.all()
+    #Selecting the event_notification queryset relating to the currently logged in user. 
+    #(on the basis of the events he has registered for)
+    user = UserProfile.objects.get(name = str(request.user))
+    team_set = user.team_set.all()
+    for team in team_set:
+        event_set = team.event_set.all()
+        for event in event_set:
+            eventnotif_set = event.eventnotification_set.all()
     eventnotif_set = eventnotif_set.distinct()
 
     #Converting  the eventnotif_set queryset object to a JSON response
